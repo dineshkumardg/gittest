7171898853f0693c0866fa3626e41d1980244baa
dineshkumardg <dineshdg87@gmail.com>
2019-08-10 21:46:53 +0530

    test

---
+++ mathematics.py
@@ -0,0 +1,27 @@
+# mathematics.py
+
+"""This is an example of a python module"""
+
+test_var = 21
+test_car = 31
+test_bar = 'I am a string'
+
+def add(a, b):
+	return a+b
+  
+def subtract(a, b):
+	return a-b
+  
+def multiply(a,b):
+	return a*b
+  
+def division(a,b):
+	return a/b
+  
+def modulo(a,b):
+	return a % b
+  
+class A:
+	x = 20
+  	y = 21
+  	z = 22
---
+++ module_examples.py
@@ -0,0 +1,12 @@
+# module_example.py
+
+import mathematics
+from mathematics import test_var, test_car, add, subtract, multiply, division
+
+
+print(add(1, 2))
+print(subtract(3, 4))
+print(dir())
+
+
+# 2 mins
